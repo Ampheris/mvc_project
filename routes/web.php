61a8854b685528c54cde33e74eb40861108aa758
Mathilda <mathilda.holmstrom@telia.com>
2021-05-09 21:24:48 +0200

    Added comments to the web.php to make it easier to read.

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,27 @@ use App\Models\Highscore;
 |
 */
 
+// Route for the index page.
 Route::get('/', function () {
     return view('index');
 });
 
+// Routes for the Dice game.
 Route::get('/diceGame', [DiceGame::class, 'index']);
 Route::post('/diceGame/updateSession', [DiceGame::class, 'updateSession']);
 
+// Route that flushes the session values and redirects backt o the frontpage.
 Route::get('/session', function () {
    session()->flush();
    return redirect('/');
 });
 
+// Route for the highscores, has database table directly connected to it in the Route.
 Route::get('/highscore', function () {
     $dice21 = Highscore::find(1);
 
     return view('highscore', ['Dice21' => $dice21]);
 });
 
+// Route for the books, has a controller instead of a directly connected database table.
 Route::get('/books', [Books::class, 'index']);
