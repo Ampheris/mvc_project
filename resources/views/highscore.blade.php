9b83c80cb956c49efef95512232578687e8e6f08
Mathilda <mathilda.holmstrom@telia.com>
2021-05-09 21:22:35 +0200

    Updated the highscore blade file.

--- resources/views/highscore.blade.php
+++ resources/views/highscore.blade.php
@@ -1,7 +1,8 @@
+{{--Page for the highscores, has no specific controller connected
+to it but a direct connection to a database table--}}
 @extends('base')
 
 @section('title', 'Highscore')
-
 
 @section('content')
     <h1>Highscore</h1>
