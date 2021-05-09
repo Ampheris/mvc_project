c3fe98a4cceea3f89ccbbec803911abbf3643628
Mathilda <mathilda.holmstrom@telia.com>
2021-05-09 21:18:59 +0200

    Updated Book model.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Book model connected to database table books.
 class Book extends Model
 {
     use HasFactory;
