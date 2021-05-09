3c283431b0c48cb0ba140a958cc66c59471742da
Mathilda <mathilda.holmstrom@telia.com>
2021-05-09 21:19:44 +0200

    Udated Highscores model

--- app/Models/Highscore.php
+++ app/Models/Highscore.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Highscore model connected to highscores table
 class Highscore extends Model
 {
     use HasFactory;
